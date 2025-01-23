3e2ce017e40ecce72cc0f411d0e9c0a8460a8515
GabrielgsdCIUwU <holagabriel321@gmail.com>
2025-01-23 13:44:19 +0100

    refactor(tabla_asignacion): Cambiar idioma de variables y funciones

--- src/tabla_asignacion.py
+++ src/tabla_asignacion.py
@@ -1,8 +1,8 @@
 class TablaAsignacion:
     
     def __init__(self):
-        self.tabla = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
+        self.table = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E']
 
-    def getTabla(self):
-        return self.tabla
-
+    def getTable(self):
+        return self.table
+     
