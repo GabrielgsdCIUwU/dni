f1a66bd18f2250f9fe1d32b7bdb86af19f893c92
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-01-24 09:20:16 +0100

    feat(dni): Añadir función isDniValid

--- src/dni.py
+++ src/dni.py
@@ -1,3 +1,4 @@
+from src.tabla_asignacion import TablaAsignacion as TA
 class Dni:
     def __init__(self, dni):
         self.dni = dni
@@ -11,3 +12,5 @@ class Dni:
     def getNumber(self):
         return self.getDni()[:-1]
 
+    def isDniValid(self):
+        return TA().calculateLetter(self.getNumber()) == self.getLetter()
