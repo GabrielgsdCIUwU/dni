3b0ac05ddad023b18399f0bceabd36124c647918
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-01-25 15:20:50 +0100

    feat(colors): Remover enum, no se utiliza

--- src/colors.py
+++ src/colors.py
@@ -1,9 +1,7 @@
-from enum import Enum
-
-class Colors(Enum):
-    RESET = (1, '\033[0m')
-    RED = (2, '\033[91m')
-    GREEN = (3, '\033[92m')
+class Colors():
+    RESET = '\033[0m'
+    RED = '\033[91m'
+    GREEN = '\033[92m'
 
     def __init__(self, code, color):
         self.code = code
